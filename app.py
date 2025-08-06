465d465f97594f0620a595f26fb5412e5c3138f0
Anushka <canushka700@gmail.com>
2025-08-06 15:11:59 +0530

    app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def recommend(movie):
 movies_dict = pickle.load(open('movie_dict.pkl','rb'))
 movies=pd.DataFrame(movies_dict)
 
-similarity = pickle.load(open('similarity.pkl','rb'))
+similarity = pickle.load(open('similarity_dict.pkl','rb'))
 
 st.title('Movie Recommender System')
 
@@ -27,6 +27,7 @@ if st.button('Recommend'):
     recommendations = recommend(selected_movie_name)
     for i in recommendations:
         st.write(i)
+
 
 
 
